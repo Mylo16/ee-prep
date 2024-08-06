745420da1fef1f1d6be80e1eef015cf2591f2e1e
Antwi Eric <ANTWI@DESKTOP-92MFH60.(none)>
2024-08-06 01:27:14 +0000

    change api URL

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -7,7 +7,7 @@ import {
   saveUserToLocalStorage,
 } from '../utils/localStorageForUser';
 
-const base_url = "https://quiz-app-backend-2tas.onrender.com";
+const base_url = "https://mysite-1gyi.onrender.com";
 
 export const userSignup = createAsyncThunk('user/signup', async (userData, { rejectWithValue }) => {
   try {
--- src/utils/api_url.js
+++ src/utils/api_url.js
@@ -1,1 +1,1 @@
-export const api_base_url = 'https://quiz-app-backend-2tas.onrender.com/api/v1';
+export const api_base_url = 'https://mysite-1gyi.onrender.com/api/v1';
